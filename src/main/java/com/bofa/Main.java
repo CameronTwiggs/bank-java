3a7d816dd5960d533734fc6eb4f5f1dc6ff754c6
CameronTwiggs <camtwiggs@gmail.com>
2022-10-25 23:52:46 -0400

    adds maven

--- src/main/java/com/bofa/Main.java
+++ src/main/java/com/bofa/Main.java
@@ -1,3 +1,5 @@
+package com.bofa;
+
 import java.util.*;
 import java.io.*;
 
@@ -8,14 +10,19 @@ class Main implements java.io.Serializable
     {
         customerMap = new HashMap <String,Customer>();
     }
+//    Makes a empty hashmap to collect a list of all of the account holders, the key is going to be a string and the value
+//    be a customer object
+
     public static void main(String []args)
     {
         Scanner sc = new Scanner(System.in);
         Customer customer;
         String username,password;double amount;
         Main bank = new Main();
+//      Main refers to the "Main" method which creates a new hashmap that will represent the bank/database
         int choice;
         outer: while(true)
+//      The "Starting point" whenever the code refers to this "outer" keyword, they are referring to this case statement.
         {
             System.out.println("\n-------------------");
             System.out.println("BANK OF JAVA");
@@ -29,6 +36,7 @@ class Main implements java.io.Serializable
             choice = sc.nextInt();
             sc.nextLine();
             switch(choice)
+//           first case switch
             {
                 case 1:
                     System.out.print("Enter name : ");
@@ -39,7 +47,9 @@ class Main implements java.io.Serializable
                     String phone = sc.nextLine();
                     System.out.println("Set username : ");
                     username = sc.next();
+
                     while(bank.customerMap.containsKey(username))
+//                    uses methods that comes from the custermap object, this checks if that username is in the hashMap
                     {
                         System.out.println("Username already exists. Set again : ");
                         username = sc.next();
@@ -48,6 +58,7 @@ class Main implements java.io.Serializable
                     password = sc.next();
                     sc.nextLine();
                     while(!password.matches((("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_]).{8,}"))))
+//                    regex for password validation.
                     {
                         System.out.println("Invalid password condition. Set again :");
                         password=sc.next();
@@ -55,7 +66,9 @@ class Main implements java.io.Serializable
                     System.out.print("Enter initial deposit : ");
                     amount=sc.nextDouble();
                     customer = new Customer(username,password,name,address,phone,amount,new Date());
+//                  "customer" variable is instantiated here.
                     bank.customerMap.put(username,customer);
+//                  this than puts that new "customer" value to the hashmap.
                     break;
                 case 2:
                     System.out.println("Enter username : ");
@@ -83,6 +96,7 @@ class Main implements java.io.Serializable
                                 choice = sc.nextInt();
                                 sc.nextLine();
                                 switch(choice)
+//                               second case switch which is nested.
                                 {
                                     case 1:
                                         System.out.print("Enter amount : ");
@@ -94,6 +108,7 @@ class Main implements java.io.Serializable
                                         amount = sc.nextDouble();
                                         sc.nextLine();
                                         customer.deposit(amount,new Date());
+//                                        customer has a method that uses the arguments passed by the user to increase the balance (.deposit())
                                         break;
                                     case 2:
                                         System.out.print("Enter payee username : ");
@@ -107,7 +122,7 @@ class Main implements java.io.Serializable
                                         }
                                         amount = sc.nextDouble();
                                         sc.nextLine();
-                                        if(amount > 300000)
+                                        if(amount > 300_000)
                                         {
                                             System.out.println("Transfer limit exceeded. Contact bank manager.");
                                             break;
@@ -115,8 +130,11 @@ class Main implements java.io.Serializable
                                         if(bank.customerMap.containsKey(username))
                                         {
                                             Customer payee = bank.customerMap.get(username);
+//                                          Once we can confirm that the targeted user exists, we assign them to payee.
                                             payee.deposit(amount,new Date());
                                             customer.withdraw(amount,new Date());
+//                                          Reminder: customer is already assigned to the one who originally signed in.
+
                                         }
                                         else
                                         {
@@ -128,6 +146,7 @@ class Main implements java.io.Serializable
                                         {
                                             System.out.println(transactions);
                                         }
+//                                      All this does is loop through the "transaction" array found in the orignal "customer" object.
                                         break;
                                     case 4:
                                         System.out.println("Accountholder name : "+customer.name);
@@ -167,6 +186,9 @@ class Main implements java.io.Serializable
                             System.out.println("-------------------\n");
                             System.out.println("1. see account information of all the customers.");
                             System.out.println("2. Log out.");
+
+//                           1. means that they are going to loop through the hashmap
+//                           2. means nothing happens.
                             System.out.print("\nEnter your choice : ");
                             choice = sc.nextInt();
                             sc.nextLine();
@@ -175,8 +197,8 @@ class Main implements java.io.Serializable
                                 case 1:
                                     System.out.println("Customers information");
                                     System.out.println("username name address phone balance");
-                                    for (Customer v : bank.customerMap.values()) {
-                                        System.out.println(v.username + " " + v.name + " " + v.address + " "+ v.phone + " " + v.balance);
+                                    for (Customer x : bank.customerMap.values()) {
+                                        System.out.println(x.username + " " + x.name + " " + x.address + " "+ x.phone + " " + x.balance);
                                     }
                                     break;
                                 case 2: continue outer;
@@ -189,15 +211,18 @@ class Main implements java.io.Serializable
 // Serialization of all the Bank details.
                     try
                     {
-//Saving of object in a file
+// Saving of object in a file
                         FileOutputStream file = new FileOutputStream(filename);
                         ObjectOutputStream out = new ObjectOutputStream(file);
                         out.writeObject(bank);
                         out.close();
                         file.close();
                         System.out.println("Object has been serialized");
+//                        Standard steps for saving data to a physical file on the machine.
+
                     }
                     catch (IOException ex) {
+
                     }
                     System.out.println("\nThank you for choosing Bank Of Java.");
                     System.exit(1);
